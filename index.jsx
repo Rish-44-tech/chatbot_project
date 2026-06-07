e55d6593b9daeadb94bf13136d69939c9fe26d32
Rishit <jalanrishit@gmail.com>
2026-06-07 17:05:47 +0000

    added styles to website and polished some features

--- index.jsx
+++ index.jsx
@@ -5,11 +5,9 @@ function ChatInput({chatMessages,setChatMessages}){
     }
 
     function sendMessage(){
-        setChatMessages([...chatMessages,{
-            message:inputVal,
-            sender:"user",
-            id:crypto.randomUUID()
-        }]);
+        if(inputVal===""){
+            return;
+        }
         const response=Chatbot.getResponse(inputVal);
         setChatMessages([...chatMessages,{
             message:inputVal,
@@ -24,29 +22,44 @@ function ChatInput({chatMessages,setChatMessages}){
     }
 
     return (
-        <>
-        <input type="text" placeholder="Send a message to Chatbot" size={30} onChange={saveInput} value={inputVal}/>
-        <button onClick={sendMessage}>Send</button>
-        <br/>Welcome to the Chatbot! Send a message using the textbox above!
-        </>
+        <div className="input-container">
+        <input
+            type="text"
+            placeholder="Send a message to Chatbot"
+            size={30} onChange={saveInput}
+            value={inputVal}
+            className="chat-input"
+            onKeyDown={(e)=>{
+                if(e.key==="Enter"){
+                    sendMessage();
+                }
+            }}/>
+        <button onClick={sendMessage} className="send-button">Send</button>
+        </div>
     )
 }
 
 function ChatMessage({message,sender}){
     return(
-        <>
-        <div className="robot-message">
-            {sender==="robot" && <img src="./robot.png" width={50}></img>}
-            {message}
-            {sender==="user" && <img src="./user.png" width={50}></img>}
+        <div className={sender+"-message"}>
+            {sender==="robot" && <img src="./robot.png" className="chat-img"></img>}
+            <div className="message-text">{message}</div>
+            {sender==="user" && <img src="./user.png" className="chat-img"></img>}
         </div>
-        </>
     );
 }
 
 function ChatMessages({chatMessages}){
+    const chatMessagesRef=React.useRef(null);
+    React.useEffect(()=>{
+        const containerElem=chatMessagesRef.current;
+        if(containerElem){
+            containerElem.scrollTop=containerElem.scrollHeight;
+        }
+    },[chatMessages])
+
     return (
-    <>
+    <div className="message-container" ref={chatMessagesRef}>
     {chatMessages.map((element)=>{
     return (
         <ChatMessage
@@ -56,19 +69,60 @@ function ChatMessages({chatMessages}){
         />
         );
     })}
-    </>
+    </div>
+    );
+}
+
+function AppBottom({setTB,chatMessages,setChatMessages}){
+     function moveTop(){
+        console.log(2);
+        setTB("top");
+     }
+
+    return (
+        <div className="app-container">
+        <ChatMessages chatMessages={chatMessages}/>
+        <ChatInput chatMessages={chatMessages} setChatMessages={setChatMessages}/>
+        <button className="pos-switch" onClick={moveTop}>Move textbox to Top</button>
+        </div>
+    );
+}
+
+function AppTop({setTB,chatMessages,setChatMessages}){
+     function moveBottom(){
+        console.log(1);
+        setTB("bottom");
+     }
+
+    return (
+        <div className="app-container">
+        <ChatInput chatMessages={chatMessages} setChatMessages={setChatMessages}/>
+        <ChatMessages chatMessages={chatMessages}/>
+        <button className="pos-switch" onClick={moveBottom}>Move textbox to Bottom</button>
+        </div>
     );
 }
 
 function App(){
-     const [chatMessages,setChatMessages]=React.useState([]);
-
-    return (
-        <>
-        <ChatInput chatMessages={chatMessages} setChatMessages={setChatMessages}/>
-        <ChatMessages chatMessages={chatMessages}/>
-        </>
-    );
+    const[TB,setTB]=React.useState("top");
+    const [chatMessages,setChatMessages]=React.useState([]);
+    if(TB==="top"){
+        return (
+            <>
+            <AppTop setTB={setTB} chatMessages={chatMessages} setChatMessages={setChatMessages}></AppTop>
+            </>
+        );
+    }
+    else if(TB==="bottom"){
+        return (
+            <>
+            <AppBottom setTB={setTB} chatMessages={chatMessages} setChatMessages={setChatMessages}></AppBottom>
+            </>
+        );
+    }
+    else{
+        return null;
+    }
 }
 
 const container = document.querySelector(".js-container");
